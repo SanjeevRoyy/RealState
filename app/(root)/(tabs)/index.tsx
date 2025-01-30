3da8ea33f0a779779470a7915635be31b6ea2ae9
SanjeevRoyy <abiralmanausya9@gmail.com>
2025-01-30 14:17:06 +0545

    database seed function done

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -4,14 +4,16 @@ import Search from "@/components/Search";
 import icons from "@/constants/icons";
 import images from "@/constants/images";
 import { useGlobalContext } from "@/lib/global-provider";
+import seed from "@/lib/seed";
 import { Link } from "expo-router";
-import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import { Button, FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   const { user } = useGlobalContext();
   return (
     <SafeAreaView className="bg-white h-full">
+      <Button title="Seed" onPress={seed} />
       <FlatList
         data={[1, 2]}
         renderItem={({ item }) => <Card />}
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -13,13 +13,13 @@ export const config = {
   platform: "com.jsm.restate",
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
-  databaseId: process.env.Expo_PUBLIC_APPWRITE_DATABASE_ID,
+  databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
   galleriesCollectionId:
-    process.env.Expo_PUBLIC_APPWRITE_GALLERIES_COLLECTION_ID,
-  reviewsCollectionId: process.env.Expo_PUBLIC_APPWRITE_REVIEWS_COLLECTION_ID,
+    process.env.EXPO_PUBLIC_APPWRITE_GALLERIES_COLLECTION_ID,
+  reviewsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_REVIEWS_COLLECTION_ID,
   propertiesCollectionId:
-    process.env.Expo_PUBLIC_APPWRITE_PROPERTIES_COLLECTION_ID,
-  agentsCollectionId: process.env.Expo_PUBLIC_APPWRITE_AGENTS_COLLECTION_ID,
+    process.env.EXPO_PUBLIC_APPWRITE_PROPERTIES_COLLECTION_ID,
+  agentsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_AGENTS_COLLECTION_ID,
 };
 export const client = new Client();
 
@@ -30,7 +30,7 @@ client
 
 export const avatar = new Avatars(client);
 export const account = new Account(client);
-export const database = new Databases(client);
+export const databases = new Databases(client);
 
 export async function Login() {
   try {
